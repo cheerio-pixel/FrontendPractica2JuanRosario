0d56159cfa33467eee7c69a05922c74849b83ae5
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2024-02-16 12:06:49 -0400

    Removing dist/

--- wwwroot/dist/types/cookies.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.COOKIES = void 0;
-/**
- * Enum of cookies
- */
-exports.COOKIES = {
-    LOGIN_EMAIL: "loginEmail"
-};
